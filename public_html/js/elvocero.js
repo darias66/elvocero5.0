805ee43edbddc17c012ae1fdf36623070e44e5ee
david <david@arias-pc.aupmerida.com>
2016-02-04 13:39:34 -0600

    Cambio del titulo del favicon

--- public_html/js/elvocero.js
+++ public_html/js/elvocero.js
@@ -1,12 +1,12 @@
 var app = angular.module('elvoceroApp',['ngRoute']);
 
 app.controller('menuCtrl', ['$scope','$http', function($scope,$http){
-        
         $scope.menuSuperior = 'fijos/encabezado.html';
         $scope.menuInferior = 'fijos/footer.html';
         
         $scope.publicidad = 'publicidadNotas.html';
         
+        $scope.titulo="El Vocero | La voz oficial de la noticia";
 
 }]);
 
